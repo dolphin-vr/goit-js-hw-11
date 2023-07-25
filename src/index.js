21f33704d1211dc18767a892c524fb5f5a969266
dolphin <fun53@ukr.net>
2023-07-25 03:21:57 +0300

    part2

--- src/index.js
+++ src/index.js
@@ -1,72 +1,78 @@
-import axios from "axios";
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// axios.defaults.baseURL = 'https://pixabay.com/api/'
-const API_KEY = '38368366-a7227dffd937457d38677860';
-// const BASE_URL = 'https://pixabay.com/api';
-
+const API_KEY = '38368366-a7227dffd937457d386778604';
+const optsSimplBox = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+const lightbox = new SimpleLightbox('.gallery a', optsSimplBox);
+let page = 1;
 const refs = {
-   form: document.querySelector('.search-form'),
-   gallery: document.querySelector('.gallery'),
-}
-
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  btnMore: document.querySelector('.more'),
+};
 refs.form.addEventListener('submit', goSearch);
-
-function goSearch(ev){
-   ev.preventDefault();
-   const serrchString = refs.form.searchQuery.value.split(" ").join("+");
-   serviceGetImages(serrchString)
+refs.btnMore.addEventListener('click', goMore);
+async function goSearch(ev) {
+  ev.preventDefault();
+  const optNotiflx = {
+    width: '380px',
+    timeout: 4000,
+    fontSize: '18px',
+  };
+  page = 1;
+  const searchString = refs.form.searchQuery.value.split(' ').join('+');
+  const images = await serviceGetImages(searchString);
+  if (images.total == 0)
+    Notify.failure('Sorry, there are no images matching your search query. Please try again.', optNotiflx);
+  refs.gallery.innerHTML = await createMarkup(images.hits);
+  lightbox.refresh();
 }
-
-async function serviceGetImages(query){
-   const config = {
-      method: 'get',
-      baseURL: 'https://pixabay.com',
-      // url: '/api',
-      params: {
-         key: API_KEY,
-         q: query,
-         image_type: 'photo',
-         orientation: 'horizontal',
-         safesearch: true,
-      }
-   }
-   // `https://pixabay.com/api?key=${API_KEY}&q=${q}`;
-   console.log(config);
-   console.log('===');
-   try {
-      const {data} = await axios('/api', config);
-      // 'https://pixabay.com/api?key=38368366-a7227dffd937457d386778604&q=cat'
-      console.log(data);
-      return data
-   } catch (error) {
-      console.log(error.message)
-   }
+async function goMore() {
+  const searchString = refs.form.searchQuery.value.split(' ').join('+');
+  const images = await serviceGetImages(searchString, ++page);
+  refs.gallery.insertAdjacentHTML('beforeend', await createMarkup(images.hits));
+  lightbox.refresh();
+  if (page >= 26) refs.btnMore.style.display = 'none';
 }
-
-function createMarkup(images){
-   return images.reduce((acc, img) => {acc + `<div class="photo-card">
-   <img src="" alt="" loading="lazy">
-   <div class="info">
-     <p class="info-item">
-       <b>Likes</b>
-     </p>
-     <p class="info-item">
-       <b>Views</b>
-     </p>
-     <p class="info-item">
-       <b>Comments</b>
-     </p>
-     <p class="info-item">
-       <b>Downloads</b>
-     </p>
-   </div>
- </div>`}, "")
+async function serviceGetImages(query, pg = 1) {
+  const config = {
+    method: 'get',
+    baseURL: 'https://pixabay.com',
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: pg,
+    },
+  };
+  try {
+    const { data } = await axios('/api', config);
+    if (page < data.totalHits) refs.btnMore.style.display = 'block';
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
-
-
-   // `https://pixabay.com/api?key=${API_KEY}&q=${q}`;
-   // &image_type=photo&orientation=horizontal&safesearch=true
-
-// axios.defaults.headers.common.Authorization = '38368366-a7227dffd937457d386778604';
-// axios.defaults.headers.common = 'Access-Control-Allow-Origin' 
-// axios.defaults.baseURL = 'https://pixabay.com'
+function createMarkup(images) {
+  const res = images
+    .map(img => {
+      return `<li class="photo-card">
+         <a class="card_link" href="${img.largeImageURL}"><img class="card_image" src="${img.webformatURL}" alt="${img.tags}" loading="lazy"></a>
+         <div class="info">
+         <p class="info-item"><b>Likes: </b>${img.likes}</p>
+         <p class="info-item"><b>Views: </b>${img.views}</p>
+         <p class="info-item"><b>Comments: </b>${img.comments}</p>
+         <p class="info-item"><b>Downloads: </b>${img.downloads}</p>
+         </div>
+      </li>`;
+    })
+    .join('');
+  return res;
+}
